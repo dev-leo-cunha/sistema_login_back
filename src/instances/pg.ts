e8035257d8e9a8d22d5ec60bc2c58636d4262af6
Leonardo Cunha <leonardo.cunha.2203@gmail.com>
2023-04-26 12:52:18 -0300

    add neondb

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -5,11 +5,22 @@ dotenv.config();
 
 // Usando o Sequelize para fazer a conexão ao postgres
 export const sequelize = new Sequelize(
-    process.env.PG_DB as string,
-    process.env.PG_USER as string,
-    process.env.PG_PASSWORD as string,
-    {
-        dialect: 'postgres',
-        port: parseInt(process.env.PG_PORT as string)
-    }
-);
+    process.env.PGDATABASE as string, 
+    process.env.PGUSER  as string, 
+    process.env.PGPASSWORD  as string, {
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Desativa a verificação SSL
+      },
+    },
+  });
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão bem-sucedida com o banco de dados.');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  });
