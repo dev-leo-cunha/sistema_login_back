ab9699fd71fffb806d319bea9317cad8fa169bf1
Leonardo Cunha <leonardo.cunha.2203@gmail.com>
2023-04-26 09:52:54 -0300

    test heroku

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,6 +6,7 @@ import * as ApiController from '../controllers/apiController';
 const router = Router();
 // definindo o nome das rotas e mandando pro ApiController
 router.get('/ping', ApiController.ping)
+router.get('/pingg', ApiController.ping)
 router.post('/register', ApiController.register);
 router.post('/login', ApiController.login);
 router.post('/access', Auth.private, ApiController.access);
